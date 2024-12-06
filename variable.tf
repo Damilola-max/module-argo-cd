ede3a3f3e48430cfec8225a60c1e750f9d241e4c
Damilola Olayemi <79061444+Damilola-max@users.noreply.github.com>
2024-12-06 05:21:51 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,15 +1,31 @@
 variable "kubernetes_cluster_id" {
   type = string
 }
+
 variable "kubernetes_cluster_cert_data" {
   type = string
 }
+
 variable "kubernetes_cluster_endpoint" {
   type = string
 }
+
 variable "kubernetes_cluster_name" {
   type = string
 }
+
 variable "eks_nodegroup_id" {
   type = string
+}
+
+provider "kubernetes" {
+  host                   = var.kubernetes_cluster_endpoint
+  client_certificate     = base64decode(var.kubernetes_cluster_cert_data)
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = var.kubernetes_cluster_name
+  }
 }
